df9f608ab6b1363932f07d393ae754280607f42b
JCValente1984 <jcr.rvalente@outlook.com>
2025-01-24 16:35:15 -0300

    Create ValidacpfApplication.java

---
+++ ValidacpfApplication.java
@@ -0,0 +1,12 @@
+package com.exemplo.validacpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidacpfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidacpfApplication.class, args);
+    }
+}
